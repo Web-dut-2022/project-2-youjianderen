c7ae21318f40ec8aeead2f34b17a53bc08799720
youjianderen <101548772+youjianderen@users.noreply.github.com>
2022-04-24 22:20:47 +0800

    Add files via upload

---
+++ commerce/auctions/admin.py
@@ -0,0 +1,16 @@
+from django.contrib import admin
+
+from .models import *
+
+class ListAdmin(admin.ModelAdmin):
+    list_display = ("__str__")
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ("__str__")
+
+class BidsAdmin(admin.ModelAdmin):
+    list_display = ("__str__")
+
+admin.site.register(List)
+admin.site.register(Bids)
+admin.site.register(Comment)
